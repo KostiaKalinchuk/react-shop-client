decaeb470afbdc2ecbf218fe6412ae08ef58e589
Your Name <9leon1@gmail.com>
2018-06-18 00:39:08 +0300

    add phone card

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 import R from 'ramda'
 import request from 'superagent'
 
-import phones from './mockPhones'
+// import phones from './mockPhones'
 import {getTotalBasketCount} from "../selectors";
 // import categories from './mockCategories'
 
+// donne
 export const fetchPhones = async () => {
     // const {body} = await request.get(
     //     'http://www.mocky.io/v2/5918b9461200001f1040dbeb'
@@ -19,35 +20,39 @@ export const fetchPhones = async () => {
 
     const phones = JSON.parse(fetch.text);
 
-    console.log(phones);
+    // console.log(phones);
 
     return phones
-
 };
 
-export const loadMorePhones = async ({offset}) => {
-    return new Promise(resolve => {
-        resolve(phones)
+// пагінація
+// export const loadMorePhones = async ({offset}) => {
+//     return new Promise(resolve => {
+//         resolve(phones)
+//     })
+// };
+
+
+// donne
+
+export const fetchPhoneById = async (id) => {
+
+    const fetch = await request.get(
+        'http://shop-api.local/phones.php'
+    );
+
+    const phones = JSON.parse(fetch.text);
+
+    console.log(phones);
+
+    return new Promise((resolve) => {
+        const phone = R.find(R.propEq('id', id), phones);
+        resolve(phone)
     })
 };
 
-export const fetchPhoneById = async (id) => {
-    // return new Promise((resolve, reject) => {
-    //     const phone = R.find(R.propEq('id', id), phones);
-    //     resolve(phone)
-    // })
-};
-
+// donne
 export const fetchCategories = async () => {
-    // return new Promise((resolve, reject) => {
-    //   resolve(categories)
-    // })
-
-    // const {body} = await request.get(
-    //     'http://www.mocky.io/v2/5b219e4430000091265c7541'
-    // );
-    // // console.log(body.categories);
-    // return body.categories
 
     const fetch = await request.get(
         'http://shop-api.local/'
@@ -55,9 +60,7 @@ export const fetchCategories = async () => {
 
     const categories = JSON.parse(fetch.text);
 
-
-    console.log(categories);
-
+    // console.log(categories);
 
     return categories
 
@@ -72,7 +75,6 @@ export const fetchCategories = async () => {
         //
         //         });
         //     });
-
 
 
         };
