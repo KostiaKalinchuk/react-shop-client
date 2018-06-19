0decebfcdec4730c4108e7be8483de3b34cba1a5
Your Name <9leon1@gmail.com>
2018-06-20 00:07:38 +0300

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = () => (
+
+        <div className='head'>
+            <div className='col-md-9'>
+                Phones market logo
+            </div>
+            <div className='col-md-3'>
+            <span className="header-phones">
+                <nobr>(044) 500-00-00, </nobr>
+                <nobr>(044) 505-89-89, </nobr>
+                <nobr>0 (800) 309-444</nobr>
+            </span>
+            </div>
+        </div>
+);
+
+export default Header
