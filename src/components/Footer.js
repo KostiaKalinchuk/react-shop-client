48aff08c2af10c613394f057fbe1bfac6f76e519
Your Name <9leon1@gmail.com>
2018-06-20 02:09:14 +0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => (
+
+    <div className='footer'>
+        <div className='col-md-12'>
+            <span className="header-phones">
+                <nobr>(044) 500-00-00, </nobr>
+                <nobr>(044) 505-89-89, </nobr>
+                <nobr>0 (800) 309-444</nobr>
+            </span>
+        </div>
+        </div>
+
+);
+
+export default Footer
